2979690fc059a1ff72df57b620a44b61ce081fb3
Dannagoni <dannagonim@gmail.com>
2024-02-23 12:28:28 -0300

    SideBar arrow

--- client/src/Components/SideBar/SideBar.jsx
+++ client/src/Components/SideBar/SideBar.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import DocumentsOptions from '../Documents/DocumetsOptions';
 
-export const Sidebar = ({ handleOptionClick, handleSubmenuDescription}) => {
+export const Sidebar = ({ handleOptionClick, handleSubmenuDescription }) => {
   const [selectedItem, setSelectedItem] = useState(null);
   const [openDashboard, setOpenDashboard] = useState(null);
-  const [selectedSubmenu, setSelectedSubmenu] = useState(null); 
+  const [selectedSubmenu, setSelectedSubmenu] = useState(null);
 
   const handleItemClick = (name) => {
-    setSelectedItem(name);
+    setSelectedItem(selectedItem === name ? null : name); // Deseleccionar si ya está seleccionado
     setSelectedSubmenu(null); // Reiniciar el submenú seleccionado cuando se hace clic en una nueva opción principal
     handleOptionClick(name);
+    toggleDashboardSubMenu(name); // Activar la transición del submenú
   };
 
   const handleSubmenuClick = (submenuTitle) => {
@@ -32,12 +33,16 @@ export const Sidebar = ({ handleOptionClick, handleSubmenuDescription}) => {
                 className={`flex items-center mb-4 rounded-lg dark:text-white hover:bg-gradient-to-br from-purple to-purple-600 dark:hover:bg-gradient-to-br from-purple to-purple-600 group ${selectedItem === option.name ? 'bg-gradient-to-br from-purple to-purple-600' : ''}`}
                 onClick={() => handleItemClick(option.name)}
               >
-                <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21" onClick={() => toggleDashboardSubMenu(option.name)}>
-                  <path fillRule="evenodd" d="M10 5a1 1 0 0 0-1 1v3H6a1 1 0 1 0 0 2h3v3a1 1 0 1 0 2 0v-3h3a1 1 0 1 0 0-2h-3V6a1 1 0 0 0-1-1Z" clipRule="evenodd" />
+                <svg className={`w-3 h-3 ml-3 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white0 dark:group-hover:text-white ${selectedItem === option.name ? 'text-white' : ''}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  {selectedItem === option.name ? (
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+                  ) : (
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                  )}
                 </svg>
                 <span className={`ms-3 ${selectedItem === option.name ? 'text-white' : 'text-gray-500'}`}>{option.name}</span>
               </a>
-              <ul className={`pl-6 space-y-2 font-medium overflow-hidden transition-all duration-300 ${openDashboard === option.name ? 'max-h-40' : 'max-h-0'}`}>
+              <ul className={`pl-4 space-y-2 font-medium overflow-hidden transition-all duration-300 ${openDashboard === option.name ? 'max-h-40' : 'max-h-0'}`}>
                 {Object.values(option.submenus).map((submenu, submenuIndex) => (
                   <li key={submenuIndex}>
                     <a
@@ -45,7 +50,6 @@ export const Sidebar = ({ handleOptionClick, handleSubmenuDescription}) => {
                       className={`pl-6 block rounded-lg dark:text-white hover:bg-gradient-to-br from-purple to-purple-600 dark:hover:bg-gradient-to-br from-purple to-purple-600 ${selectedItem === option.name && selectedSubmenu === submenu.title ? 'bg-gradient-to-br from-purple to-purple-600 text-white' : 'text-gray-500'}`}
                       onClick={() => {
                         handleSubmenuClick(submenu.title);
-                        // handleOptionClick(option.name);
                       }}
                     >
                       {submenu.title}
