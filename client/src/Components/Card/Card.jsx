1e4e732de57c4c2a28de0b486e920118edd06c92
Dannagoni <dannagonim@gmail.com>
2024-02-29 16:29:30 -0300

    changes

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -6,7 +6,6 @@ const Card = ({ name, description, category, price, image }) => {
       <img className="w-full" src={image} alt={name} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
-        <p className="text-gray-700 text-base">{description}</p>
         <p className="text-gray-700 text-base">Category: {category}</p>
         <p className="text-gray-700 text-base">Price: {price}</p>
       </div>
